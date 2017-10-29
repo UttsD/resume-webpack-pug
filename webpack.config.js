f4ff6418bf531b1245f0ab3ecf0b072d25c295e1
Dariya Utts <Utts_d@outlook.com>
2017-10-29 17:57:55 +0300

    try css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+'use strict';
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 const PATHS = {
     source: path.join(__dirname, 'source'),
@@ -7,7 +9,7 @@ const PATHS = {
 };
 
 module.exports = {
-    entry: PATHS.source + '/index.pug',
+    entry: PATHS.source + '/main.js',
     output: {
         path: PATHS.build,
         filename: '[name].js'
@@ -18,14 +20,23 @@ module.exports = {
         })
     ],
     module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
+        
+        loaders: [{
+            test: /\.pug$/,
+            loader: 'pug-loader',
+            options: {
+                pretty: true
             }
-        ]
+        }, {
+            test: /\.css$/,
+            loader: 'style!css!autoprefixer?browsers=last 2 versions'
+        }, {
+            test: /\.(png|svg|ttf|eot|woff|woff2)$/,
+            loader: 'file?name=[path].[ext]'
+        }]
+        
+    },
+    devServer: {
+        stats: 'errors-only'
     }
 };
